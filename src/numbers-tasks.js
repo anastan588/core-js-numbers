2bca374fc3f9f7b9577af357ded1cbbc2e238c24
Anastasiya Andronova <anastasiya5.88@mail.ru>
2023-12-25 21:32:58 +0300

    feat: add solution of number task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -277,8 +277,21 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) {
+    return 0;
+  }
+
+  let previous = 0;
+  let current = 1;
+
+  for (let i = 2; i <= index; i += 1) {
+    const next = previous + current;
+    previous = current;
+    current = next;
+  }
+
+  return current;
 }
 
 /**
