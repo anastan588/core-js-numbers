0f7e6b66dd1df953f53cbdc68570f693c9a01542
Anastasiya Andronova <anastasiya5.88@mail.ru>
2023-12-25 21:51:20 +0300

    feat: add solutions of number task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -589,8 +589,9 @@ function getIntegerPartNumber(number) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+function getSumOfNumbers(x1, x2, x3) {
+  const sum = x1 + x2 + x3;
+  return parseFloat(sum.toFixed(1));
 }
 
 /**
